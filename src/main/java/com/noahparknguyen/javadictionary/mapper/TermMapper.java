e932bd6b3f138ed1ea3b7be9744ac246e824c41c
NoahPN <noahparknguyen@gmail.com>
2026-04-26 00:50:24 -0400

    Added new mapper methods

--- src/main/java/com/noahparknguyen/javadictionary/mapper/TermMapper.java
+++ src/main/java/com/noahparknguyen/javadictionary/mapper/TermMapper.java
@@ -1,15 +1,17 @@
 package com.noahparknguyen.javadictionary.mapper;
 
 import com.noahparknguyen.javadictionary.dto.request.CreateTermRequest;
+import com.noahparknguyen.javadictionary.dto.request.UpdateTermRequest;
 import com.noahparknguyen.javadictionary.dto.response.TermResponse;
 import com.noahparknguyen.javadictionary.model.Term;
 import org.springframework.stereotype.Component;
 
-import java.util.Set;
+import java.util.HashSet;
 
 @Component
 public class TermMapper {
-    public  TermResponse toResponse(Term term) {
+
+    public TermResponse toResponse(Term term) {
         return new TermResponse(
                 term.getId(),
                 term.getName(),
@@ -22,11 +24,19 @@ public class TermMapper {
 
     public Term toEntity(CreateTermRequest request) {
         Term term = new Term();
+        applyCommonFields(term, request);
+        return term;
+    }
+
+    public void updateEntityFromRequest(Term term, UpdateTermRequest request) {
+        applyCommonFields(term, request);
+    }
+
+    private void applyCommonFields(Term term, CreateTermRequest request) {
         term.setName(request.getName());
         term.setCasualDefinition(request.getCasualDefinition());
         term.setFormalDefinition(request.getFormalDefinition());
         term.setExperienceLevel(request.getExperienceLevel());
-        term.setTags(request.getTags() != null ? request.getTags() : Set.of());
-        return term;
+        term.setTags(request.getTags() != null ? request.getTags() : new HashSet<>());
     }
 }
