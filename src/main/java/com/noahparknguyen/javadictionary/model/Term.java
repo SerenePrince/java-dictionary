47f9e171c23e163e819645217aa214428223d825
NoahPN <noahparknguyen@gmail.com>
2026-04-25 23:15:23 -0400

    Updated model annotations

--- src/main/java/com/noahparknguyen/javadictionary/model/Term.java
+++ src/main/java/com/noahparknguyen/javadictionary/model/Term.java
@@ -1,28 +1,38 @@
 package com.noahparknguyen.javadictionary.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "terms")
 @Getter
 @Setter
+@NoArgsConstructor
 public class Term {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
+    @NotBlank
     private String name;
 
     @Column(nullable = false, length = 500)
+    @NotBlank
+    @Size(max = 500)
     private String casualDefinition;
 
     @Column(nullable = false, length = 1000)
+    @NotBlank
+    @Size(max = 1000)
     private String formalDefinition;
 
     @Enumerated(EnumType.STRING)
@@ -32,8 +42,5 @@ public class Term {
     @ElementCollection
     @CollectionTable(name = "term_tags", joinColumns = @JoinColumn(name = "term_id"))
     @Column(name = "tag")
-    private Set<String> tags;
-
-    public Term() {
-    }
+    private Set<String> tags = new HashSet<>();
 }
