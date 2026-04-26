65de8c8904323e0096a40369f930df293ee900b6
NoahPN <noahparknguyen@gmail.com>
2026-04-26 00:53:05 -0400

    Switched from H2 to PSQL

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,43 @@
+-- src/main/resources/schema.sql
+
+CREATE TABLE IF NOT EXISTS terms
+(
+    id
+    BIGSERIAL
+    PRIMARY
+    KEY,
+    name
+    VARCHAR
+(
+    100
+) NOT NULL UNIQUE,
+    casual_definition VARCHAR
+(
+    500
+) NOT NULL,
+    formal_definition VARCHAR
+(
+    1000
+) NOT NULL,
+    experience_level VARCHAR
+(
+    20
+) NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS term_tags
+(
+    term_id
+    BIGINT
+    NOT
+    NULL
+    REFERENCES
+    terms
+(
+    id
+) ON DELETE CASCADE,
+    tag VARCHAR
+(
+    255
+) NOT NULL
+    );
