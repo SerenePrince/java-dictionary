9bc54a7382c1539a5befb0c7f398bccea505c2ce
NoahPN <noahparknguyen@gmail.com>
2026-04-26 00:47:12 -0400

    Converted TermResponse class into a record

--- src/main/java/com/noahparknguyen/javadictionary/dto/response/TermResponse.java
+++ src/main/java/com/noahparknguyen/javadictionary/dto/response/TermResponse.java
@@ -1,18 +1,15 @@
 package com.noahparknguyen.javadictionary.dto.response;
 
 import com.noahparknguyen.javadictionary.model.ExperienceLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 import java.util.Set;
 
-@Getter
-@AllArgsConstructor
-public class TermResponse {
-    private Long id;
-    private String name;
-    private String casualDefinition;
-    private String formalDefinition;
-    private ExperienceLevel experienceLevel;
-    private Set<String> tags;
+public record TermResponse(
+        Long id,
+        String name,
+        String casualDefinition,
+        String formalDefinition,
+        ExperienceLevel experienceLevel,
+        Set<String> tags
+) {
 }
