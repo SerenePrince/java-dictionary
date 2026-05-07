5e53e9644cff757ca6bff89f534e3b6e7a6b8e03
NoahPN <noahparknguyen@gmail.com>
2026-05-07 02:45:21 -0400

    Added new queries

--- src/main/java/com/noahparknguyen/javadictionary/repository/TermRepository.java
+++ src/main/java/com/noahparknguyen/javadictionary/repository/TermRepository.java
@@ -3,6 +3,8 @@ package com.noahparknguyen.javadictionary.repository;
 import com.noahparknguyen.javadictionary.model.ExperienceLevel;
 import com.noahparknguyen.javadictionary.model.Term;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,7 +19,15 @@ public interface TermRepository extends JpaRepository<Term, Long> {
 
     List<Term> findByTagsContaining(String tag);
 
-    List<Term> findByNameContainingIgnoreCase(String keyword);
+    @Query("SELECT t FROM Term t WHERE " +
+           "LOWER(t.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+           "LOWER(t.casualDefinition) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+           "LOWER(t.formalDefinition) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Term> searchByKeyword(@Param("keyword") String keyword);
 
-    List<Term> findByExperienceLevelAndNameContainingIgnoreCase(ExperienceLevel level, String keyword);
+    @Query("SELECT t FROM Term t WHERE t.experienceLevel = :level AND (" +
+           "LOWER(t.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+           "LOWER(t.casualDefinition) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+           "LOWER(t.formalDefinition) LIKE LOWER(CONCAT('%', :keyword, '%')))")
+    List<Term> searchByKeywordAndLevel(@Param("keyword") String keyword, @Param("level") ExperienceLevel level);
 }
