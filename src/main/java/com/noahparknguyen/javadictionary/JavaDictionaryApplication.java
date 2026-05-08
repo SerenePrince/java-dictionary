3a170002f4d033e7d912959fcfd7cb21af2702bc
NoahPN <noahparknguyen@gmail.com>
2026-05-07 20:07:52 -0400

    Added configuration properties

--- src/main/java/com/noahparknguyen/javadictionary/JavaDictionaryApplication.java
+++ src/main/java/com/noahparknguyen/javadictionary/JavaDictionaryApplication.java
@@ -1,9 +1,12 @@
 package com.noahparknguyen.javadictionary;
 
+import com.noahparknguyen.javadictionary.config.roadmap.RoadmapProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RoadmapProperties.class)
 public class JavaDictionaryApplication {
 
     public static void main(String[] args) {
