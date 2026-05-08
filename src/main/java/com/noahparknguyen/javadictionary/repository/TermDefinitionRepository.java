97df096284d4eba4c589f4f0de6538673d43dfa6
NoahPN <noahparknguyen@gmail.com>
2026-05-07 20:07:06 -0400

    Removed unused query

--- src/main/java/com/noahparknguyen/javadictionary/repository/TermDefinitionRepository.java
+++ src/main/java/com/noahparknguyen/javadictionary/repository/TermDefinitionRepository.java
@@ -4,11 +4,7 @@ import com.noahparknguyen.javadictionary.model.ExperienceLevel;
 import com.noahparknguyen.javadictionary.model.TermDefinition;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface TermDefinitionRepository extends JpaRepository<TermDefinition, Long> {
-
-    Optional<TermDefinition> findByTermIdAndExperienceLevel(Long termId, ExperienceLevel level);
 
     boolean existsByTermIdAndExperienceLevel(Long termId, ExperienceLevel level);
 }
